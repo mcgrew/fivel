7f6614cf93910dca0894ead93a18af70adbbce29
Thomas McGrew <tjmcgrew@gmail.com>
2014-10-02 01:59:03 -0400

    Added hook scripts to the build process
    These scripts are needed for cordova to handle the build process properly when
      symlinks are present in www.
    Also modified the script so it is intended to be run from the root directory
      of the project.

---
+++ 050_fivel_before_build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mv www www~
+cp -rL www~ www
+
--- setup.sh
+++ setup.sh
@@ -5,27 +5,34 @@ echo "Linking files from $FIVEL_DIR..."
 
 mkdir -p js css html img
 
-ln -s ../$FIVEL_DIR/css/login.css css/login.css
-ln -s ../$FIVEL_DIR/css/mobileapp.css css/mobileapp.css
-ln -s ../$FIVEL_DIR/html/login.html html/login.html
-ln -s ../$FIVEL_DIR/img/active_arrow.png img/active_arrow.png
-ln -s ../$FIVEL_DIR/img/active_arrow.svg img/active_arrow.svg
-ln -s ../$FIVEL_DIR/img/back_icon.png img/back_icon.png
-ln -s ../$FIVEL_DIR/img/back_icon.svg img/back_icon.svg
-ln -s ../$FIVEL_DIR/img/home.png img/home.png
-ln -s ../$FIVEL_DIR/img/home.svg img/home.svg
-ln -s ../$FIVEL_DIR/img/logout.png img/logout.png
-ln -s ../$FIVEL_DIR/img/logout.svg img/logout.svg
-ln -s ../$FIVEL_DIR/img/menu_icon.png img/menu_icon.png
-ln -s ../$FIVEL_DIR/img/menu_icon.svg img/menu_icon.svg
-ln -s ../$FIVEL_DIR/img/trash.png img/trash.png
-ln -s ../$FIVEL_DIR/img/trash.svg img/trash.svg
-ln -s ../$FIVEL_DIR/img/x.png img/x.png
-ln -s ../$FIVEL_DIR/img/x.svg img/x.svg
-ln -s    $FIVEL_DIR/index.html index.html
-ln -s ../$FIVEL_DIR/js/jquery.min.js js/jquery.min.js
-ln -s ../$FIVEL_DIR/js/jquery.mustache.js js/jquery.mustache.js
-ln -s ../$FIVEL_DIR/js/mobileapp.js js/mobileapp.js
+ln -s ../../$FIVEL_DIR/css/login.css www/css/login.css
+ln -s ../../$FIVEL_DIR/css/mobileapp.css www/css/mobileapp.css
+ln -s ../../$FIVEL_DIR/html/login.html www/html/login.html
+ln -s ../../$FIVEL_DIR/img/active_arrow.png www/img/active_arrow.png
+ln -s ../../$FIVEL_DIR/img/active_arrow.svg www/img/active_arrow.svg
+ln -s ../../$FIVEL_DIR/img/back_icon.png www/img/back_icon.png
+ln -s ../../$FIVEL_DIR/img/back_icon.svg www/img/back_icon.svg
+ln -s ../../$FIVEL_DIR/img/home.png www/img/home.png
+ln -s ../../$FIVEL_DIR/img/home.svg www/img/home.svg
+ln -s ../../$FIVEL_DIR/img/logout.png www/img/logout.png
+ln -s ../../$FIVEL_DIR/img/logout.svg www/img/logout.svg
+ln -s ../../$FIVEL_DIR/img/menu_icon.png www/img/menu_icon.png
+ln -s ../../$FIVEL_DIR/img/menu_icon.svg www/img/menu_icon.svg
+ln -s ../../$FIVEL_DIR/img/trash.png www/img/trash.png
+ln -s ../../$FIVEL_DIR/img/trash.svg www/img/trash.svg
+ln -s ../../$FIVEL_DIR/img/x.png www/img/x.png
+ln -s ../../$FIVEL_DIR/img/x.svg www/img/x.svg
+ln -s    ../$FIVEL_DIR/index.html www/index.html
+ln -s ../../$FIVEL_DIR/js/jquery.min.js www/js/jquery.min.js
+ln -s ../../$FIVEL_DIR/js/jquery.mustache.js www/js/jquery.mustache.js
+ln -s ../../$FIVEL_DIR/js/mobileapp.js www/js/mobileapp.js
+
+# add hooks to handle the symlinks in the www directory.
+mkdir -p hooks/before_build hooks/before_run hooks/after_build hooks/after_run
+ln -s ../../$FIVEL_DIR/050_before_build.sh hooks/before_build/
+ln -s ../../$FIVEL_DIR/050_before_build.sh hooks/before_run/
+ln -s ../../$FIVEL_DIR/050_after_build.sh hooks/after_build/
+ln -s ../../$FIVEL_DIR/050_after_build.sh hooks/after_run/
 
 
 echo "Creating/copying other required files..."
